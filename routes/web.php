5ac4001be9677c20660ed909e54a9196b2991044
Abdurrahman Ziyad <abdurrahman.ziyad17@gmail.com>
2024-05-18 15:15:29 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardProductController;
+use App\Http\Controllers\DashboardSettingController;
+use App\Http\Controllers\DashboardTransactionController;
+use App\Http\Controllers\DetailController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController; // alias untuk DashboardController di dalam namespace Admin
+use App\Http\Controllers\DashboardController as UserDashboardController; // alias untuk DashboardController di luar namespace Admin
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories');
+Route::get('/details/{id}', [DetailController::class, 'index'])->name('detail');
+Route::get('/cart', [CartController::class, 'index'])->name('cart');
+Route::get('/success', [CartController::class, 'success'])->name('success');
+Route::get('/register/success', [RegisterController::class, 'success'])->name('register-success');
+Route::get('/dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
+// dashboard product
+Route::get('/dashboard/products', [DashboardProductController::class, 'index'])->name('dashboard-product');
+Route::get('/dashboard/products/create', [DashboardProductController::class, 'create'])->name('dashboard-product-create');
+Route::get('/dashboard/products/{id}', [DashboardProductController::class, 'details'])->name('dashboard-product-details');
+// transaction
+Route::get('/dashboard/transactions', [DashboardTransactionController::class, 'index'])->name('dashboard-transaction');
+Route::get('/dashboard/transactions/{id}', [DashboardTransactionController::class, 'details'])->name('dashboard-transaction-details');
+// settings
+Route::get('/dashboard/settings', [DashboardSettingController::class, 'store'])->name('dashboard-settings');
+Route::get('/dashboard/account', [DashboardSettingController::class, 'account'])->name('dashboard-account');
+
+Route::prefix('admin')->namespace('Admin')->group(function () {
+    Route::get('/', [AdminDashboardController::class, 'index'])->name('admin-dashboard');
+});
